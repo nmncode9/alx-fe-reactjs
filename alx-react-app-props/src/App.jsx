f9161168c93d2556ecf7f6a4db6a946226aa1a49
numan <197640394+nmncode9@users.noreply.github.com>
2025-07-19 20:08:20 +0100

    Refactor UserProfile to use context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@ import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext'
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com", bio: "One simple man for one simple task." };
 
   return (
     <UserContext.Provider value={{userData}} >
